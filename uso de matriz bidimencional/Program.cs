3211590f64f2595d3b54a67381dcbe9adda0bbd0
Jose Castro <ustr005624.ugb.edu.sv>
2024-09-13 18:36:08 -0600

    Agregar archivos de proyecto.

---
+++ uso de matriz bidimencional/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uso_de_matriz_bidimencional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uso de matrices.
+            int[,] matriz = new int[,] {
+                {5,4,5,2,1},
+                {1,2,8,9,3}
+            };
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("{0} ", matriz[i, j]);
+                }
+                Console.WriteLine("\n");
+            }
+            //Pausa
+            Console.ReadKey();
+        }
+    }
+}
